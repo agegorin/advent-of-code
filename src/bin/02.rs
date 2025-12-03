afbd8e72b3f8281204905eef0a33bd126d0df3d8
Egor <agegorin@gmail.com>
2025-12-03 14:46:07 +0100

    solve hard 02

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -24,6 +24,30 @@ fn is_valid(number: u64) -> bool {
     number != right_number + right_number * 10u64.pow(half_length as u32)
 }
 
+fn is_valid2(number: u64) -> bool {
+    let string_number = number.to_string();
+    let length = string_number.len();
+    for sub_length in 1..=(length / 2) {
+        if length.is_multiple_of(sub_length) {
+            let substring = string_number.get(..sub_length).unwrap();
+
+            let mut is_same = true;
+            for index in 0..length / sub_length {
+                is_same &= string_number
+                    .get(index * sub_length..(index + 1) * sub_length)
+                    .unwrap()
+                    == substring;
+            }
+
+            if is_same {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let mut result = 0;
 
@@ -39,8 +63,19 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(result)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut result = 0;
+
+    let values = get_values(input);
+    for (left, right) in values {
+        for number in left..=right {
+            if !is_valid2(number) {
+                result += number;
+            }
+        }
+    }
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -56,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
